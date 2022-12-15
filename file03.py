a505dacc82ab7bc388c738ab95d0ab3d6a38059c
user1 <backenduz@gmail.com>
2022-12-16 00:14:01 +0500

    isdigit

--- file03.py
+++ file03.py
@@ -1,4 +1,4 @@
-def main(data:str):
+def main(data):
     """
     The data is from the file. Return the numbers as a list type.
     Args:
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    ls = []
+    for i in range(len(data)):
+        if data[i].isdigit():
+            ls.append(data[i])
+    return ls
 # Read data from file
+f = open('txt_file/data03.txt', 'r')
+r = f.read()
+print(main(r))
