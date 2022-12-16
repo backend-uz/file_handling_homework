4dfd90d783baede9d792e224ae9e16f6e42b19d9
user1 <backenduz@gmail.com>
2022-12-16 20:45:47 +0500

    len alpha and num

--- file05.py
+++ file05.py
@@ -6,5 +6,21 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    num = []
+    al = []
+
+    for i in data:
+        if i.isdigit():
+            num.append(i)
+    for i in data:
+        if not i.isdigit():
+            al.append(i)
+            
+    ls = []
+    ls.append(len(num))
+    ls.append(len(al))
+    return ls
 # Read data from file
+f = open('txt_file/data05.txt', 'r')
+r = f.read()
+print(main(r))
