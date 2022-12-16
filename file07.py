60dfc627c70ad46987650154ec4b017625426c90
user1 <backenduz@gmail.com>
2022-12-16 23:15:46 +0500

    sum

--- file07.py
+++ file07.py
@@ -1,4 +1,4 @@
-def main(data:str):
+def main(data):
     """
     The data is from the file. Find a sum of numeric characters and return as list type.
     Args:
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    
+    sum = 0
+    for i in range(len(data)):
+        if data[i].isdigit():
+            sum = sum + int(data[i])
+    return sum
 # Read data from file
+f = open('txt_file/data07.txt', 'r')
+r = f.read()
+print(main(r))
