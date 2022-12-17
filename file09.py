162a05b5d383e186f0d4bba7dea7bbfac4fa3fe8
user1 <backenduz@gmail.com>
2022-12-17 13:41:41 +0500

    min

--- file09.py
+++ file09.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    mn = '0'
+    for i in range(len(data)):
+        if data[i].isdigit:
+            if data[i] < mn:
+                mn += data[i]
+    return mn
+            
 # Read data from file
+f = open('txt_file/data09.txt', 'r')
+r = f.read()
+print(main(r))
